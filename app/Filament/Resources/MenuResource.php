1a05374b13da90e126f46add5faa72aa63dc7b2d
Ajay Rai <rai.invisibleweb@gmail.com>
2025-07-20 15:29:54 +0545

    feat: menu crud & position

---
+++ app/Filament/Resources/MenuResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MenuResource\Pages;
+use App\Filament\Resources\MenuResource\RelationManagers;
+use App\Models\Menu;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class MenuResource extends Resource
+{
+    protected static ?string $model = Menu::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-bars-3';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Menu')
+                    ->schema([
+
+                        TextInput::make('name')
+                            ->live()
+                            ->afterStateUpdated(function (string $operation, string $state, Forms\Set $set) {
+                                if ($operation == 'edit') {
+                                    return;
+                                }
+                                $set('slug', Str::slug($state));
+                            })
+                            ->required()
+                            ->rules('string|max:50')
+                            ->columnSpanFull(),
+                        Select::make('menu_position')
+                            ->options([
+                                'top' => 'Top',
+                                'quick-links' => 'Quick Links',
+                                'important-links' => 'Important Links'
+                            ]),
+                        TextInput::make('slug')->unique(ignoreRecord: true)->required(),
+                        Toggle::make('status')
+                    ])->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('slug')->sortable()->searchable(),
+                TextColumn::make('menu_position')->sortable(),
+                ToggleColumn::make('status')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMenus::route('/'),
+            'create' => Pages\CreateMenu::route('/create'),
+            'edit' => Pages\EditMenu::route('/{record}/edit'),
+        ];
+    }
+}
